063a0c181e6abf970ed061d82cd63b63e41238ed
Ales <aleswebgit@gmail.com>
2022-08-13 21:52:18 +0200

    index connect to mongodb

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = process.env.MONGODB_URI
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server started on port ${PORT}`)))
+  .catch(err => console.log(err.message))
